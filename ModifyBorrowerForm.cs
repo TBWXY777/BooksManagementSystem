b81b3c5f57c9b363768b6d88ef5e5ee44d0ad2de
vrossi710816 <86088549+vrossi710816@users.noreply.github.com>
2021-06-25 10:40:12 +0800

    读者的增删查改

---
+++ ModifyBorrowerForm.cs
@@ -0,0 +1,85 @@
+﻿using ConnectSql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace BooksManagementSystem
+{
+    public partial class ModifyBorrowerForm : Form
+    {
+        public ModifyBorrowerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ModifyBorrowerForm_Load(object sender, EventArgs e)
+        {
+            MySql.Data.MySqlClient.MySqlConnection con;
+            con = MysqlUtils.GetMySqlConnection();
+            con.Open();
+            MySqlCommand command = new MySqlCommand();
+            command.CommandText = "select * from reader where r_id='" + textBox1 + "'";
+            var reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                reader.Read();
+                //textBox1.Text = reader["r_id"].ToString();
+                textBox2.Text = reader["r_name"].ToString();
+                textBox3.Text = reader["major"].ToString();
+                textBox5.Text = reader["dep"].ToString();
+                textBox4.Text = reader["borrowed"].ToString();
+                textBox6.Text = reader["allow_borrow"].ToString();
+                textBox7.Text = reader["r_pwd"].ToString();
+            }
+            else
+            {
+                MessageBox.Show("该读者不存在");
+                this.Close();
+
+            }
+            reader.Close();
+            con.Close();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySql.Data.MySqlClient.MySqlConnection con;
+            con = MysqlUtils.GetMySqlConnection();
+            con.Open();
+            MySqlCommand command = new MySqlCommand();
+            command.CommandText = "select * from reader where r_id='" + textBox1 + "'";
+            var reader = command.ExecuteReader();
+            command.CommandText = "update reader set " +
+                   "r_name='" + textBox2.Text +
+                   "',major='" + textBox3.Text +
+                   "',dep='" + textBox4.Text +
+                   "',borrowed='" + textBox5.Text +
+                   "',allow_borrow='" + textBox6.Text +
+                   "',r_pwd='" + textBox7.Text +
+                   "' where r_id='" + textBox1 + "'";
+            if (command.ExecuteNonQuery() != 0)
+            {
+                MessageBox.Show("修改成功！");
+                con.Close();
+            }
+            else
+            {
+                MessageBox.Show("修改失败！");
+                con.Close();
+            }
+
+        }
+    }
+}
